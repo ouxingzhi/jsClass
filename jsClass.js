efac12722a77ec6ee8205f2628ae0ab6e007f18b
ouxingzhi <ouxingzhi@vip.qq.com>
2013-04-23 11:43:57 +0800

    Update jsClass.js

--- jsClass.js
+++ jsClass.js
@@ -10,20 +10,25 @@ function Class(supClass, subProperty) {
         supClass = function () {};
     }
     var slice = Array.prototype.slice,
-    supProto = supClass.prototype,
-    emptyClass = function () {},
-    newClass = function () {
-        this.initialize.apply(this, arguments);
-    },
-    supInitialize,
-    subInitialize,
-    i;
+    	supProto = supClass.prototype,
+		emptyClass = function () {},
+		newClass = function () {
+			this.__propertys__();
+			this.initialize.apply(this, arguments);
+		},
+		sup__propertys__,
+		sub__propertys__,
+		supInitialize,
+		subInitialize,
+		i;
     emptyClass.prototype = supProto;
     newClass.prototype = new emptyClass();
     newClass.prototype.constructor = supClass;
     supInitialize = newClass.prototype.initialize || function () {};
     subInitialize = subProperty.initialize || function () {};
-    var argslist = /function\s*\(([^\(\)]*?)\)\{/i.exec(subInitialize.toString())[1].replace(/\s/i, '').split(',');
+    sup__propertys__ = newClass.prototype.__propertys__ || function () {};
+    sub__propertys__ = subProperty.__propertys__ || function () {};
+    var argslist = /^\s*function\s*\(([^\(\)]*?)\)\s*?\{/i.exec(subInitialize.toString())[1].replace(/\s/i, '').split(',');
     for (i in subProperty) {
         subProperty.hasOwnProperty(i) && (newClass.prototype[i] = subProperty[i]);
     }
@@ -35,7 +40,12 @@ function Class(supClass, subProperty) {
                 }
             ];
             subInitialize.apply(this, args.concat(slice.call(arguments)));
-        }
+        };
     }
+    //预设成员定义区
+    newClass.prototype.__propertys__ = function () {
+        sup__propertys__.call(this);
+        sub__propertys__.call(this);
+    };
     return newClass;
 }
